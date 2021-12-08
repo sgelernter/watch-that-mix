aaa91a97323388254101ce3a8f79d02590136148
sam <s.gelernter@gmail.com>
2021-12-08 12:14:38 -0500

    Add CSS Reset

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // import _ from 'lodash';
 import { _ } from 'core-js';
+import './reset.css';
 import { EffectPage } from './effect_page.js';
 import * as imports from './imports.js';
 
@@ -78,48 +79,3 @@ document.addEventListener("DOMContentLoaded", (event) => {
     update();
     
 });
-
-
-// const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-// circle.setAttribute('cx', '50%');
-// circle.setAttribute('cy', '50%');
-// circle.setAttribute('r', '50');
-// circle.setAttribute('fill', 'blue');
-// circle.setAttribute('id', 'dynamic-circle');
-// chartContainer.append(circle);
-// let currentPage = new EffectPage(audioContext, 'comp');
-// let newPage = new EffectPage(audioContext, 'eq');
-
-
-
-// createButtons();
-
-// const rightFull = imports.createAudioNode('Correct Compression', './audio/compression/Teacup_rightcompression_fulltrack.wav', false);
-// const rightSolo = imports.createAudioNode('Solo Track/Correct Compression', './audio/compression/Teacup_rightcompression_solo.wav');
-// const noFull = imports.createAudioNode('No Compression', './audio/compression/Teacup_nocomp_fulltrack.wav');
-// const noSolo = imports.createAudioNode('Solo Track/No Compression', './audio/compression/Teacup_nocomp_solo.wav');
-// const overFull = imports.createAudioNode('Over-Compression', './audio/compression/Teacup_overcompress_fulltrack.wav');
-// const overSolo = imports.createAudioNode('Solo Track/Over-Compression', './audio/compression/Teacup_overcompress_solo.wav');
-// const context = new AudioContext();
-// const RFTrack = context.createMediaElementSource(rightFull);
-// const RSTrack = context.createMediaElementSource(rightSolo); 
-// const NFTrack = context.createMediaElementSource(noFull); 
-// const NSTrack = context.createMediaElementSource(noSolo); 
-// const OFTrack = context.createMediaElementSource(overFull); 
-// const OSTrack = context.createMediaElementSource(overSolo); 
-// const fullNodes = [rightFull, noFull, overFull];
-// const soloNodes = [rightSolo, noSolo, overSolo];
-// const pageNodes = fullNodes.concat(soloNodes);
-// const pageTracks = [RFTrack, RSTrack, NFTrack, NSTrack, OFTrack, OSTrack];
-// pageTracks.forEach (function(track) {track.connect(context.destination)});
-// // debugger
-// const playButton = imports.playButton(context, pageNodes);
-// const soloButton = new imports.SoloToggle(fullNodes, soloNodes);
-// // debugger
-// document.body.append(playButton);
-// document.body.append(soloButton);
-
-// fullNodes.forEach (function(node) {
-//     document.body.append(document.createElement('br'));
-//     document.body.append(imports.sourceToggler(node, fullNodes, soloNodes, soloButton));
-// })
